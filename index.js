80ad1f05870a1c5d0abbcebf92b09aa72d646780
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-01-17 11:17:33 -0300

    rendering the views

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 
+
 const app = express()
 const port = 8000;
 
+// Template engine
+const { engine } = require('express-handlebars');
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
 //Routers
 app.get('/', function(req, res) {
-  res.send('O aplicativo está funcionando')
+  res.render('home');
 })
+
 
 app.listen(port, () => {
   console.log(`project running on the door: ${port}`);

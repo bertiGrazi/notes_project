727d0971024d874fdc58a805553b0ed5dd649df4
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-01-23 19:48:56 -0300

    check if password is igual to confirmpassword and if user exists

--- party_time/backend/routes/authRoutes.js
+++ party_time/backend/routes/authRoutes.js
@@ -18,16 +18,16 @@ router.post("/register", async (req, res) => {
   }
 
   //check if password match
-  // if (password != confirmpassword) {
-  //   return res.status(400).json({ error: "As senhas não conferem!"});
-  // }
+  if (password != confirmpassword) {
+    return res.status(400).json({ error: "As senhas não conferem!"});
+  }
 
     //check if user exists
-    // const emailExits = await User.findOne({email: email});
+    const emailExits = await User.findOne({email: email});
 
-    // if(emailExits) {
-    //   return res.status(400).json({ error: "O e-mail informado já está em uso!"});
-    // }
+    if(emailExits) {
+      return res.status(400).json({ error: "O e-mail informado já está em uso!"});
+    }
 });
 
 module.exports = router;

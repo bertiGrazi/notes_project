c086355417e939f8b23292992bab5755aece022b
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-01-24 11:56:01 -0300

    inicial update user

--- party_time/backend/routes/userRoutes.js
+++ party_time/backend/routes/userRoutes.js
@@ -6,6 +6,9 @@ const User = require("../models/user");
 //middlewares 
 const verifyToken = require("../helpers/check-token");
 
+//helpers
+const getUserByToken = require("../helpers/get-user-by-token"); 
+
 //get an user
 router.get("/:id", verifyToken, async(req, res) => {
   //api/user/DH#4*HRDH#4*HRDH#4*HR
@@ -27,6 +30,19 @@ router.get("/:id", verifyToken, async(req, res) => {
 
 //update an user
 router.put("/",  verifyToken, async (req, res) => {
+  const token = req.header("auth-token");
+  const user = await getUserByToken(token); 
+
+  const userReqId = req.body.id;
+  const password = req.body.password;
+  const confirmpassword = req.body.confirmpassword;
+
+  const userId = user._id.toString();
+
+  //ckeck if user id is equal token user id
+  if(userId != userReqId) {
+    res.status(401).json({error: "Acesso negado! "});
+  }
 
 }); 
 module.exports = router;

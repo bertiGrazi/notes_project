0af9d8f637fb593b9010386e4106dd2cea46960a
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-01-24 10:24:16 -0300

    fix: passwordHash when create an user

--- party_time/backend/routes/authRoutes.js
+++ party_time/backend/routes/authRoutes.js
@@ -13,76 +13,105 @@ router.post("/register", async (req, res) => {
   const confirmpassword = req.body.confirmpassword;
 
   //check for required fields
-  if(name == null || email == null || password == null || confirmpassword == null) {
-    return res.status(400).json({ error: "Por favor, preencha todos os campos!"});
+  if (
+    name == null ||
+    email == null ||
+    password == null ||
+    confirmpassword == null
+  ) {
+    return res
+      .status(400)
+      .json({ error: "Por favor, preencha todos os campos!" });
   }
 
   //check if password match
   if (password != confirmpassword) {
-    return res.status(400).json({ error: "As senhas não conferem!"});
+    return res.status(400).json({ error: "As senhas não conferem!" });
   }
 
-    //check if user exists
-    const emailExits = await User.findOne({email: email});
-    if(emailExits) {
-      return res.status(400).json({ error: "O e-mail informado já está em uso!"});
-    }
+  //check if user exists
+  const emailExits = await User.findOne({ email: email });
+  if (emailExits) {
+    return res
+      .status(400)
+      .json({ error: "O e-mail informado já está em uso!" });
+  }
 
-    // create password
-    const salt = await bcrypt.genSalt(12);
-    const passwordHash = await bcrypt.hash(password, salt);
-    //console.log(passwordHash)
+  // create password
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
+  //console.log(passwordHash)
 
-    const user = new User({
-      name: name, 
-      email: email, 
-      password, passwordHash
+  const user = new User({
+    name: name,
+    email: email,
+    password: passwordHash
+  });
+
+  try {
+    const newUser = await user.save();
+
+    //create token
+    const token = jwt.sign(
+      // payload
+      {
+        name: newUser.name,
+        id: newUser._id,
+      },
+      "nossoscret"
+    );
+
+    //return token
+    res.json({
+      error: null,
+      msg: "Você realizou o cadastro com sucesso",
+      token: token,
+      userId: newUser._id,
     });
-
-    try {
-      const newUser = await user.save();
-
-      //create token
-      const token = jwt.sign(
-        // payload 
-        {
-          name: newUser.name,
-          id: newUser._id
-        }, 
-        "nossoscret"
-      );
-
-      //return token 
-      res.json({
-        error: null,
-        msg: "Você realizou o cadastro com sucesso", 
-        token: token,
-        userId: newUser._id 
-      })
-    } catch(error) {
-      res.status(400).json({ error });
-    }
-
+  } catch (error) {
+    res.status(400).json({ error });
+  }
 });
 
 //login an user
-router.post("/login", async(req, res) => {
+router.post("/login", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
   //check if user exist
-  const user = await User.findOne({ email: email});
+  const user = await User.findOne({ email: email });
 
   if (!user) {
-    return res.status(400).json({ error: "Não há um usuário cadastrado com esse e-mail!"});
+    return res
+      .status(400)
+      .json({ error: "Não há um usuário cadastrado com esse e-mail!" });
   }
 
   //check if password match
   const checkPassword = await bcrypt.compare(password, user.password);
   if (!checkPassword) {
-    return res.status(400).json({ error: "Senha inválida!"});
+    return res
+    .status(400)
+    .json({ error: "Senha inválida!" });
   }
 
+  //create token
+  const token = jwt.sign(
+    // payload
+    {
+      name: user.name,
+      id: user._id,
+    },
+    "nossoscret"
+  );
+
+  //return token
+  res.json({
+    error: null,
+    msg: "Você está autenticado!",
+    token: token,
+    userId: user._id,
+  });
 });
 
 module.exports = router;

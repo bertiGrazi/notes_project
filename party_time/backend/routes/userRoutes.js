a9587a727ae37070b411f35cf7de8dbec0ec37c9
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-01-24 10:52:27 -0300

    feat: get an user router

--- party_time/backend/routes/userRoutes.js
+++ party_time/backend/routes/userRoutes.js
@@ -4,9 +4,23 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user");
 
 //get an user
-router.get("/", async(req, res) => {
-  res.json({
-    msg: "Funcionou!"
-  });
+router.get("/:id", async(req, res) => {
+  //api/user/DH#4*HRDH#4*HRDH#4*HR
+  const id = req.params.id;
+
+  //verify user
+  try {
+      // password: 0 = tirando o dado que não precisamos que nos retorne
+    const user = await User.findOne({ _id: id }, { password: 0 });
+    res.json({
+      error: null, 
+      user
+    });
+
+  } catch(err) {
+    return res.status(400).json({ error: "O usuário não existe!"}); 
+  }
+
+
 });
 module.exports = router;

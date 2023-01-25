b1893d18d95df9958b8987e94222870ed2a54ca8
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2023-01-25 19:15:12 -0300

    create new party

--- party_time/backend/routes/partyRoutes.js
+++ party_time/backend/routes/partyRoutes.js
@@ -7,16 +7,76 @@ const User = require("../models/user");
 
 //define file storage 
 const diskStorage = require("../helpers/file-storage");
-const upload = multer({ storage: diskStorage })
+const upload = multer({ storage: diskStorage });
 // middlewares
 const verifyToken = require("../helpers/check-token");
 
 // helpers
 const getUserByToken = require("../helpers/get-user-by-token");
 
-// get party 
-router.get("/", verifyToken, (req, res) => {
-  res.json({msg: "Funcionou! "})
-})
+// create new party
+router.post("/", verifyToken, upload.fields([{name: "photos"}]), async (req, res) => {
+
+  // req data
+  const title = req.body.title;
+  const description = req.body.description;
+  const partyDate = req.body.party_date;
+
+  let files = [];
+
+  if(req.files) {
+    files = req.files.photos;
+  }
+
+  // validations
+  if(title == "null" || description == "null" || partyDate == "null") {
+    return res.status(400).json({ error: "Preencha pelo menos nome, descrição e data." });
+  }
+
+  // verify user 
+  const token = req.header("auth-token");
+
+  const userByToken = await getUserByToken(token);
+  
+  const userId = userByToken._id.toString(); 
+
+  const user = await User.findOne({ _id: userId });
+
+  if (!user) {
+    return res.status(400).json({ error: "O usuário não existe!" });
+  }
+
+  // create photos array with path
+  let photos = [];
+
+  if(files && files.length > 0) {    
+
+    files.forEach((photo, i) => {
+      photos[i] = photo.path;
+    });
+
+  }
+
+  const party = new Party({
+    title: title,
+    description: description,
+    partyDate: partyDate,
+    photos: photos,
+    privacy: req.body.privacy,
+    userId: userId
+  });
+
+  try {      
+
+      const newParty = await party.save();
+      res.json({ error: null, msg: "Evento criado com sucesso!", data: newParty });
+
+  } catch (error) {
+
+      res.status(400).json({ error })
+      
+  }
+
+});
 
 module.exports = router;
